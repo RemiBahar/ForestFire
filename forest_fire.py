4a02b3a12ec8433e7b07a411834dd7ff276f0370
RemiBahar <32700264+RemiBahar@users.noreply.github.com>
2020-10-29 17:14:06 +0000

    Add files via upload
    -Simulation now plots empty, burning, tree sites as a function of time
    -Fixed issue where many trees would burn at once

--- forest_fire.py
+++ forest_fire.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Oct 26 09:33:07 2020
-
 @author: remib
 """
 
@@ -10,19 +9,25 @@ import numpy as np
 import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 from matplotlib import animation
+from time import sleep
 
 #User-defined variables
 initial_p_tree = 1 #Probability of each site having a tree initially
 p = 0.1 #Probability of a tree being grown in an empty site
 f = 0.01 #Probability of a tree burning even if there are no nearby burning trees
 sidelength = 50
+run_iteration = 100
 
 empty, tree, burning = 0,1,2
 
 #Generate intial set of trees
 tree_array = np.random.choice([0,1], size=(sidelength,sidelength), p=[1-initial_p_tree, initial_p_tree])
 
+burning_tree_array, normal_tree_array, empty_tree_array = np.array([]), np.array([]), np.array([])
+
 def update_tree_array(tree_array):
+    global burning_tree_array, normal_tree_array, empty_tree_array
+    
     tree_array_old = tree_array
     tree_array = np.zeros((sidelength,sidelength))
     row_counter = -1
@@ -62,15 +67,40 @@ def update_tree_array(tree_array):
                 tree_array[row_counter][col_counter] = empty
             
             col_counter = col_counter + 1
-    print("burning, tree, empty")        
-    print(np.count_nonzero(tree_array==burning))
-    print(np.count_nonzero(tree_array==tree))
-    print(np.count_nonzero(tree_array==empty))
+   
+    burning_tree_array = np.append(burning_tree_array, np.count_nonzero(tree_array==burning))
+    normal_tree_array = np.append(normal_tree_array, np.count_nonzero(tree_array==tree))
+    empty_tree_array = np.append(empty_tree_array, np.count_nonzero(tree_array==empty))
+    
     return tree_array
 
+def analyse_data(burning_tree_array, tree_array, empty_tree_array):
+    plt.figure()
+    plt.title("History")
+    plt.plot(burning_tree_array, label="Burning")
+    plt.plot(normal_tree_array, label="Trees")
+    plt.plot(empty_tree_array, label="Empty")
+    plt.xlabel("Time")
+    plt.ylabel("Number")
+    plt.legend()
+    plt.show()
+    
+
 def animate(i):
-    im.set_data(animate.X)
-    animate.X = update_tree_array(animate.X)
+    global iteration
+    if iteration < run_iteration:
+        iteration = iteration + 1
+        print(iteration)
+        im.set_data(animate.X)
+        animate.X = update_tree_array(animate.X)
+    else:
+        analyse_data(burning_tree_array, tree_array, empty_tree_array)
+        print("Finished Animation")
+        
+        
+        
+
+        
 
 color_array =["black", "green", "red"] 
 cmap_test = colors.ListedColormap(color_array) 
@@ -87,7 +117,9 @@ im = ax.imshow(tree_array, cmap=cmap_test, norm=norm_test)#, interpolation='near
 animate.X = tree_array
 
 # Interval between frames (ms).
-interval = 1000
-anim = animation.FuncAnimation(fig, animate, interval=interval)
+interval = 100
+iteration = 0
+
+anim = animation.FuncAnimation(fig, animate, frames=run_iteration, interval=interval, repeat=False)
 plt.draw()
 plt.show()
