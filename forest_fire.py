b9771b4a0fee367e72a186ba95669e5b1eb89427
RemiBahar <32700264+RemiBahar@users.noreply.github.com>
2020-10-29 15:53:02 +0000

    Add files via upload

--- forest_fire.py
+++ forest_fire.py
@@ -12,9 +12,9 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 
 #User-defined variables
-initial_p_tree = 0.2 #Probability of each site having a tree initially
-p_growing_tree = 0.02 #Probability of a tree being grown in an empty site
-p_tree_burning = 0.02 #Probability of a tree burning even if there are no nearby burning trees
+initial_p_tree = 1 #Probability of each site having a tree initially
+p = 0.1 #Probability of a tree being grown in an empty site
+f = 0.01 #Probability of a tree burning even if there are no nearby burning trees
 sidelength = 50
 
 empty, tree, burning = 0,1,2
@@ -23,40 +23,49 @@ empty, tree, burning = 0,1,2
 tree_array = np.random.choice([0,1], size=(sidelength,sidelength), p=[1-initial_p_tree, initial_p_tree])
 
 def update_tree_array(tree_array):
+    tree_array_old = tree_array
+    tree_array = np.zeros((sidelength,sidelength))
     row_counter = -1
     col_counter = 0
 
-    for row in tree_array:
+    for row in tree_array_old:
         row_counter = row_counter + 1
         col_counter = 0
     
         for site in row:
-
+            initial_state = site
             if site == empty:
                 #Randomly populate empty site with tree
-                tree_array[row_counter][col_counter] = np.random.choice([empty,burning], size=1, p=[1-p_growing_tree, p_growing_tree])[0]
+                tree_array[row_counter][col_counter] = np.random.choice([empty,tree], size=1, p=[1-p, p])[0]
+                if tree_array[row_counter][col_counter] == burning:
+                    print("burning randomly again")
             elif site == tree:
                 #If one of direct neighbour burning then burn this tree
                 #If tree above burning
-                if row_counter != 0 and tree_array[row_counter-1][col_counter] == burning:
+                if row_counter != 0 and tree_array_old[row_counter-1][col_counter] == burning:
                     tree_array[row_counter][col_counter] = burning
                 #If tree below burning
-                elif row_counter != (sidelength -1) and tree_array[row_counter+1][col_counter] == burning:
+                elif row_counter != (sidelength -1) and tree_array_old[row_counter+1][col_counter] == burning:
                     tree_array[row_counter][col_counter] = burning
                 #If tree to the left burning
-                elif col_counter != 0 and tree_array[row_counter][col_counter-1] == burning:
+                elif col_counter != 0 and tree_array_old[row_counter][col_counter-1] == burning:
                     tree_array[row_counter][col_counter] = burning
                 #If tree to the right burning
-                elif col_counter != (sidelength-1) and tree_array[row_counter][col_counter+1] == burning:
+                elif col_counter != (sidelength-1) and tree_array_old[row_counter][col_counter+1] == burning:
                     tree_array[row_counter][col_counter] = burning
                 #Otherwise randomly burn trees
                 else:
-                 tree_array[row_counter][col_counter] = np.random.choice([tree,burning], size=1, p=[1-p_tree_burning, p_tree_burning])[0]
+                 tree_array[row_counter][col_counter] = np.random.choice([tree,burning], size=1, p=[1-f, f])[0]
+                 
             elif site==burning:
                 #Burning tree become empty site
                 tree_array[row_counter][col_counter] = empty
             
             col_counter = col_counter + 1
+    print("burning, tree, empty")        
+    print(np.count_nonzero(tree_array==burning))
+    print(np.count_nonzero(tree_array==tree))
+    print(np.count_nonzero(tree_array==empty))
     return tree_array
 
 def animate(i):
@@ -78,7 +87,7 @@ im = ax.imshow(tree_array, cmap=cmap_test, norm=norm_test)#, interpolation='near
 animate.X = tree_array
 
 # Interval between frames (ms).
-interval = 100
+interval = 1000
 anim = animation.FuncAnimation(fig, animate, interval=interval)
 plt.draw()
 plt.show()
