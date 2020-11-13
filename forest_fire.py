41d5e8ba3aa7b9b3242d159257cf063ed7c6d13a
RemiBahar <32700264+RemiBahar@users.noreply.github.com>
2020-11-13 14:25:14 +0000

    Add files via upload
    -Improved performance by using random.choices to generate probability outcomes for growing a tree or a tree spontaneously burning at the beginning of the simulation instead of as the simulation runs
    -Fixed Hoshen-Kopelman algorithm and tested it to ensure cluster labelling is correct
    
    -Further improvements needed by employing the quick union-find algorithm possibly with path compression

--- forest_fire.py
+++ forest_fire.py
@@ -1,18 +1,16 @@
-#Import modules
-#%matplotlib notebook #needed for animations in jupyter
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 12 17:43:20 2020
+
+@author: remib
+"""
+#Import modules 
 import numpy as np
 import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import tkinter as tk #for user forms
-
-#Arrays of form labels and predefined values
-fields = 'Mode (animation or analysis)', 'Iterations to run', 'Initial p_tree', 'p', 'f', 'tree rows', 'tree columns', 'interval'
-#Array of pre-defined values for running program
-#predefined_values = np.array(["animation",100,1,0.1,0.01,10,10,2000])
-predefined_values = np.array(["analysis",3000,1,0.1,0.001,200,200,2000])
-
-#Functions
+from random import choices
 
 #Executed when user clicks run button
 def fetch(entries):
@@ -47,6 +45,45 @@ def makeform(window, fields):
         i=i+1
         
     return entries
+        
+#Generate user pop-up window
+fields = 'Mode (animation or analysis)', 'Iterations to run', 'Initial p_tree', 'p', 'f', 'tree rows', 'tree columns', 'interval'
+predefined_values = np.array(["analysis",3000,1,0.1,0.001,50,50,2000])
+#predefined_values = np.array(["animation",100,1,0.1,0.001,10,10,2000])
+
+if __name__ == '__main__':
+    #Generate pop-up window
+    window = tk.Tk() #creates window
+    ents = makeform(window, fields) #makes form
+    window.bind('<Return>', (lambda event, e=ents: fetch(e))) #closes window
+    b1 = tk.Button(window, text='Run',
+                  command=(lambda e=ents: fetch(e))) #run button
+    b1.pack(side=tk.LEFT, padx=5, pady=5)
+    b2 = tk.Button(window, text='Cancel', command=window.quit) #cancel button
+    b2.pack(side=tk.LEFT, padx=5, pady=5)
+    window.mainloop() #goes to main python file
+    
+#User-defined variables
+mode= user_input_array[0] #animation or analysis
+run_iteration = int(user_input_array[1]) #Number of times to run simulation. 15s for 100 iterations. Need 3000 for cluster analysis
+p_initial = float(user_input_array[2]) #Probability of each site having a tree initially
+p = float(user_input_array[3]) #Probability of a tree being grown in an empty site
+f = float(user_input_array[4]) #Probability of a tree burning even if there are no nearby burning trees
+rows = int(user_input_array[5])
+columns = int(user_input_array[6])
+interval = float(user_input_array[7]) #Interval between animation frames
+
+iteration = 0
+#Randomly generates initial forest
+total_iterations = (rows*columns*run_iteration)+1
+tree_array = np.random.choice([0,1], size=(rows,columns), p=[1-p_initial, p_initial]) 
+empty_p_array = choices([0,1], [1-p, p], k=total_iterations)
+tree_f_array =  choices([1,2], [1-f, f], k=total_iterations)
+site_counter = 0
+cluster_frequency_array = np.zeros(int(rows*columns)+1)
+burning_array = np.array([])
+normal_tree_array = np.array([])
+empty_array = np.array([])
 
 def hk(site_matrix, site_matrix_rows, site_matrix_columns):
     """
@@ -77,11 +114,11 @@ def hk(site_matrix, site_matrix_rows, site_matrix_columns):
     for i in range(site_matrix_rows): #loop through rows
         for j in range(site_matrix_columns): #loop through columns
             if site_matrix[i][j] == 1: #only label occupied sites
-                
+
                 #Get left and above sites
                 left = 0 if j == 0 else site_matrix[i][j-1] 
                 above = 0 if i == 0 else site_matrix[i-1][j]
-                
+    
                 #If there are no occupied sites above or on the left, create a new equivalence class
                 if left != 1 and above != 1: 
                     largest_equivalence_class = largest_equivalence_class + 1
@@ -95,10 +132,17 @@ def hk(site_matrix, site_matrix_rows, site_matrix_columns):
                     cluster_matrix[i][j] = cluster_matrix[i-1][j]
                 #If there are occupied sites above and on the left, merge these two equivalence classes    
                 else:
-                    left_root_node_index = int(cluster_matrix[i][j-1]-1) 
-                    right_root_node_index = int(cluster_matrix[i-1][j]-1)
-                    equivalence_array[left_root_node_index] = equivalence_array[right_root_node_index]
-                    cluster_matrix[i][j] = cluster_matrix[i][j-1]
+                    cluster_matrix[i][j] = cluster_matrix[i-1][j]
+                    if cluster_matrix[i-1][j] != cluster_matrix[i][j-1]:
+                        left_root_node = equivalence_array[int(cluster_matrix[i][j-1]-1)]
+                        above_root_node = equivalence_array[int(cluster_matrix[i-1][j]-1)]
+                       
+                        c=-1
+                        for e in equivalence_array:
+                            c=c+1
+                            if equivalence_array[c] == left_root_node:
+                                equivalence_array[c] = above_root_node
+
     
     #Raster scan grid and assign correct labels to sites                
     for i in range(site_matrix_rows):
@@ -106,150 +150,100 @@ def hk(site_matrix, site_matrix_rows, site_matrix_columns):
             if cluster_matrix[i][j] != 0:
                 #Use the equivalence class to label the site
                 cluster_matrix[i][j] = equivalence_array[int(cluster_matrix[i][j]-1)]
-     
+    
     hk_result_array = [cluster_matrix, equivalence_array]       
     
     return hk_result_array
 
-
-if __name__ == '__main__':
-    #Generate pop-up window
-    window = tk.Tk() #creates window
-    ents = makeform(window, fields) #makes form
-    window.bind('<Return>', (lambda event, e=ents: fetch(e))) #closes window
-    b1 = tk.Button(window, text='Run',
-                  command=(lambda e=ents: fetch(e))) #run button
-    b1.pack(side=tk.LEFT, padx=5, pady=5)
-    b2 = tk.Button(window, text='Cancel', command=window.quit) #cancel button
-    b2.pack(side=tk.LEFT, padx=5, pady=5)
-    window.mainloop() #goes to main python file
-
-#User-defined variables
-mode= user_input_array[0] #animation or analysis
-run_iteration = int(user_input_array[1]) #Number of times to run simulation. 15s for 100 iterations. Need 3000 for cluster analysis
-initial_p_tree = float(user_input_array[2]) #Probability of each site having a tree initially
-p = float(user_input_array[3]) #Probability of a tree being grown in an empty site
-f = float(user_input_array[4]) #Probability of a tree burning even if there are no nearby burning trees
-#Forest dimensions
-x = int(user_input_array[5])
-y = int(user_input_array[6])
-interval = float(user_input_array[7]) #Interval between animation frames
-
-#Other variables
-iteration = 0
-empty, tree, burning = 0,1,2 #State of a site
-tree_array = np.random.choice([0,1], size=(x,y), p=[1-initial_p_tree, initial_p_tree]) #Generates initial forest
-cluster_frequency_array = np.zeros(int((x*y)+1))
-burning_tree_array, normal_tree_array, empty_tree_array = np.array([]), np.array([]), np.array([])
-cluster_array = np.zeros((x,y))
-
-def update_tree_array(tree_array):
-    
-    #Allows function to access variables outside its scope
-    global burning_tree_array, normal_tree_array, empty_tree_array, cluster_array
-    global cluster_frequency_array, largest_label, iteration
-    cluster_array = np.zeros((x,y))
-    #Define variables
-    tree_array_old = tree_array
-    tree_array = np.zeros((x,y)) #Generate new tree array to replace previous one
-    row_counter = -1 #y position of site
-    col_counter = 0 #x position of site
-    largest_label = 0 #For cluster labelling
-    
-    #For cluster label testing
-    """
+def update_tree_array(matrix):
+    global x, y, f, p, rows, columns, site_counter, ax, cluster_frequency_array
+    global burning_array, empty_array, normal_tree_array
+    #Create variable to store updated forest
+    tree_array_old = matrix 
+    matrix = np.zeros((rows, columns)) 
+                
+    hk_result_array = hk(tree_array_old, rows, columns)
+    cluster_array = hk_result_array[0]
+    label_array = hk_result_array[1]            
+    """         
     for t in ax.texts:
         t.set_visible(False)
+    
+    for i in range(rows):
+        for j in range(columns):
+            text = ax.text(x=j, y=i,s= cluster_array[i][j],ha="center", va="center", color="w")  
+     
     """
-    
-    #Loop through rows and update sites
-    for row in tree_array_old:
-        row_counter = row_counter + 1 #Increment x position
-        col_counter = 0 #Reset y position
-        
-        #Loop through columns in a row
-        for site in row:
-            if site == empty:
-                #Randomly populate empty site with tree
-                tree_array[row_counter][col_counter] = np.random.choice([empty,tree], size=1, p=[1-p, p])[0]
-            elif site == tree:
-                #If one of direct neighbour burning then burn this tree
-                #If tree above burning
-                if row_counter != 0 and tree_array_old[row_counter-1][col_counter] == burning:
-                    tree_array[row_counter][col_counter] = burning
-                #If tree below burning
-                elif row_counter != (y -1) and tree_array_old[row_counter+1][col_counter] == burning:
-                    tree_array[row_counter][col_counter] = burning
-                #If tree to the left burning
-                elif col_counter != 0 and tree_array_old[row_counter][col_counter-1] == burning:
-                    tree_array[row_counter][col_counter] = burning
-                #If tree to the right burning
-                elif col_counter != (x-1) and tree_array_old[row_counter][col_counter+1] == burning:
-                    tree_array[row_counter][col_counter] = burning
-                #Otherwise randomly burn trees
-                else:
-                     tree_array[row_counter][col_counter] = np.random.choice([tree,burning], size=1, p=[1-f, f])[0]
-            elif site==burning:
-                #Burning tree become empty site
-                tree_array[row_counter][col_counter] = empty
-            
-            col_counter = col_counter + 1
-    
-    #After updating sites 
-    burning_tree_array = np.append(burning_tree_array, np.count_nonzero(tree_array==burning))
-    normal_tree_array = np.append(normal_tree_array, np.count_nonzero(tree_array==tree))
-    empty_tree_array = np.append(empty_tree_array, np.count_nonzero(tree_array==empty))
-   
-    row_counter = -1 #y position of site
-    col_counter = 0 #x position of site
-    
-    largest_label = 0
-    id_array = np.arange((x*y))
-    label_array = id_array
-    
-    hk_result_array = hk(tree_array_old, x, y)
-    cluster_array = hk_result_array[0]
-    label_array = hk_result_array[1]
-    """
-    for row in cluster_array:
-        row_counter = row_counter + 1
-        col_counter = 0
-        for site in cluster_array:
-            
-            #For testing cluster labelling
-            text = ax.text(x=col_counter, y=row_counter,s= cluster_array[row_counter][col_counter],ha="center", va="center", color="w")  
-            col_counter = col_counter + 1
-    """
-    #Count size of each cluster and increment counters for each cluster size
     unique_label_array = list(set(label_array))
     for label in unique_label_array:
         cluster_size = np.count_nonzero(cluster_array==label)
         cluster_frequency_array[cluster_size] = cluster_frequency_array[cluster_size] + 1
- 
-    return tree_array
-
-def animate(i):
-   
-    global iteration #Allows function to access iteration variable
     
-    if iteration < run_iteration:
-        iteration = iteration + 1 #Increment iteration
-        im.set_data(animate.X) #Updates animation
-        animate.X = update_tree_array(animate.X) #Calculates new animation data
-    else:
-        print("Finished Animation. Producing plots")
-        analyse_data()
-              
-        
-def analyse_data(): 
-    print("Hello")
-    global burning_tree_array, normal_tree_array, empty_tree_array, x, y, cluster_frequency_array, tree_density
+    #Raster scan grid
+    for i in range(rows):
+        for j in range(columns):
+            site_counter += 1
+            if tree_array_old[i][j] == 0: #site empty
+                #Randomly populate empty site with tree
+                #matrix[i][j] = np.random.choice([0,1], size=1, p=[1-p, p])[0]
+                matrix[i][j] = empty_p_array[site_counter]
+            elif tree_array_old[i][j] == 1: #site occupied by tree
+                #Get neighbouring sites
+                above = 0 if i == 0 else tree_array_old[i-1][j]
+                right = 0 if j == (columns-1) else tree_array_old[i][j+1]
+                below = 0 if i == (rows-1) else tree_array_old[i+1][j]
+                left = 0 if j == 0 else tree_array_old[i][j-1]
+                
+                #Burn site if neighbouring site is burning
+                if above == 2 or right == 2 or below == 2 or left == 2:
+                    matrix[i][j] = 2
+                #Otherwise site has a probability f of burning    
+                else:
+                    matrix[i][j] = tree_f_array[site_counter]
+            #If site burning becomes empty
+            else: 
+                matrix[i][j] = 0
+                
+    #After updating sites 
+    burning_array = np.append(burning_array, np.count_nonzero(tree_array==2))
+    normal_tree_array = np.append(normal_tree_array, np.count_nonzero(tree_array==1))
+    empty_array = np.append(empty_array, np.count_nonzero(tree_array==0))
+           
+    return matrix
+    
+def analyse_data():
+    global rows, columns, normal_tree_array, cluster_frequency_array, p, f, p_initial, burning_array
+    global empty_array
+    tree_density = np.mean(tree_array[0:-1])/(rows*columns)
+    
+    #Update log files
+    file = open("Data/cluster_array.txt", "w")
+    np.savetxt(file, cluster_frequency_array)
+    file.close()
+
+    summary_array = np.array([p,f,run_iteration,rows,columns,p_initial, tree_density])
+    file = open("Data/summary_data.txt", "w")
+    np.savetxt(file, summary_array)
+    file.close()
+    
+    file = open("Data/burning_array.txt", "w")
+    np.savetxt(file, burning_array)
+    file.close()
+    
+    file = open("Data/tree_array.txt", "w")
+    np.savetxt(file, normal_tree_array)
+    file.close()
+    
+    file = open("Data/empty_array.txt", "w")
+    np.savetxt(file, empty_array)
+    file.close()
+    
     #Plot graph of site evolution
     plt.figure()
     plt.title("Evolution of grid")
-    plt.plot(burning_tree_array, label="Burning", color="orange")
+    plt.plot(burning_array, label="Burning", color="orange")
     plt.plot(normal_tree_array, label="Trees", color="green")
-    plt.plot(empty_tree_array, label="Empty", color="black")
+    plt.plot(empty_array, label="Empty", color="black")
     plt.xlabel("Time")
     plt.ylabel("Number")
     plt.legend()
@@ -271,38 +265,31 @@ def analyse_data():
     plt.grid(which="both")
     #plt.savefig("ClusterSizeFrequency.png", dpi=1200, format="png")
     plt.show()
-    
-    #Calculate important stats
-    total_sites=x*y
-    mean_trees = np.mean(normal_tree_array[0:-1])
-    tree_density = mean_trees/total_sites
-    print("tree density: ", tree_density)
-    
-    file = open("Data/ClusterArray.txt", "w")
-    np.savetxt(file, cluster_frequency_array)
-    file.close()
+        
 
-    summary_array = np.array([p,f,run_iteration,x,y,initial_p_tree,tree_density])
-    file = open("Data/SummaryData.txt", "w")
-    np.savetxt(file, summary_array)
-    file.close()
+def animate(i):
     
-    file = open("Data/BurningTreeArray.txt", "w")
-    np.savetxt(file, burning_tree_array)
-    file.close()
+    global iteration #Allows function to access iteration variable
     
-    file = open("Data/NormalTreeArray.txt", "w")
-    np.savetxt(file, normal_tree_array)
-    file.close()
-    
-    file = open("Data/EmptyTreeArray.txt", "w")
-    np.savetxt(file, empty_tree_array)
-    file.close()
-    
-    print("Finished")
-
+    if iteration < run_iteration:
+        iteration = iteration + 1 #Increment iteration
+        im.set_data(animate.X) #Updates animation
+        animate.X = update_tree_array(animate.X) #Calculates new animation data
+    else:
+        print("Finished Animation. Producing plots")
+        analyse_data()
+        
 #Runs animation
-if mode=="animation":
+if mode == "analysis":
+    
+    while iteration < run_iteration:
+        iteration = iteration +1
+        print(iteration)
+        tree_array = update_tree_array(tree_array) #Calculates new site data
+    print("Finished calculating data")
+    analyse_data()
+#Analyses data
+else: 
     #Sets colours of animation        
     colors_list = [(0.2,0,0), (0,0.5,0), (1,0,0), 'orange']
     cmap_test = colors.ListedColormap(colors_list)
@@ -318,12 +305,3 @@ if mode=="animation":
     plt.draw()
     plt.axis('off')
     plt.show()
-#Analyses data
-elif mode=="analysis":
-    while iteration < run_iteration:
-        iteration = iteration +1
-        print(iteration)
-        tree_array = update_tree_array(tree_array) #Calculates new site data
-    print("Finished calculating data")
-    analyse_data()
-    
